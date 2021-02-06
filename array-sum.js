f754514a6322e6e734d579599057ee13fd96b9b5
joinalzx <joinalzx@gmail.com>
2021-02-06 16:38:18 +0600

    array sum

---
+++ array-sum.js
@@ -0,0 +1,26 @@
+// function getArraySum (numbers){
+//     var sum = 0;
+//     for (var i = 0; i<numbers.length; i++){
+//         var elements = numbers[i];
+//         sum = sum + elements;
+// }
+// return sum;
+// }
+// var numbers = [45, 65,78,12,3,54,65];
+// var result = getArraySum(numbers);
+// var total = getArraySum([12,23,34,45,23,56]);
+// console.log("Total of the numbers: ", result);
+// console.log("This is the next numbers: ",total)
+
+// var friends = ["Alu", "Bulu", "culu", "fulu"]
+// var allNames = "";
+// for (var i = 0; i<friends.length; i++){
+//     var  names = friends[i];
+//     allNames = allNames ++;
+// }
+// console.log(allNames)
+
+function add (a, b){
+    return a + b ;
+
+} console.log(add("adam" + "eve"))
